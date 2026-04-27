514eef6c53b39ed6ed8cc9c28877b463c5806274
Swastik Garg <gargswastik06@gmail.com>
2026-04-27 10:38:44 +0530

    Added custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Swastik Garg | Portfolio",
   description: "Personal portfolio of Swastik Garg",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
